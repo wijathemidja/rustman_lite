e337f0a249e366159e8e5465b8d4cb14c3ef1655
Rahul Wijesurendra <wijaking@outlook.com>
2026-03-08 13:06:41 +0000

    RELEASE v4 Added CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,22 @@
 use std::collections::HashMap;
-use std::{fs, io, vec};
+use std::env;
 use std::fs::File;
 use std::io::Write;
+use std::{fs, vec};
 
-fn main(){
-    let mut chosen_operation = false;
-    let mut operation = String::new();
-    while chosen_operation == false {
-        println!("Would you like to encode or decode?");
-        io::stdin().read_line(&mut operation).expect("Failed to read line");
-        if &operation.trim().to_lowercase() == "encode"{
-            chosen_operation = true;
-            encode();
-        } else if &operation.trim().to_lowercase() == "decode"{
-            chosen_operation = true;
-            decode();
-        }
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    if &args[1].trim().to_lowercase() == "encode" {
+        println!("Encoding");
+        encode(args[2].to_string(), args[3].to_string());
+    } else if &args[1].trim().to_lowercase() == "decode" {
+        decode(args[2].to_string());
+    } else {
+        println!("Unknown operation");
     }
-
-
 }
 
-fn encode() {
-    let mut input: String = String::new();
-    println!("What's the message that you would like to encode?");
-    // Reads user input
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+fn encode(input:String, path:String) {
     // Trims input string
     let input_string = { input.trim() };
     // Passes input to single char function
@@ -58,8 +49,14 @@ fn encode() {
         let huffman_len = huffman_list.len();
         let mut key1 = huffman_list[huffman_len - 1][0].trim();
         let mut key2 = huffman_list[huffman_len - 2][0].trim();
-        let value1 = huffman_list[huffman_len - 1][1].trim().parse::<i32>().unwrap();
-        let value2 = huffman_list[huffman_len - 2][1].trim().parse::<i32>().unwrap();
+        let value1 = huffman_list[huffman_len - 1][1]
+            .trim()
+            .parse::<i32>()
+            .unwrap();
+        let value2 = huffman_list[huffman_len - 2][1]
+            .trim()
+            .parse::<i32>()
+            .unwrap();
         let weight = value1 + value2;
         let weight = weight.to_string();
         if key1 == "" {
@@ -104,12 +101,11 @@ fn encode() {
         println!("{} {}", key, val);
         let string = String::from(format!("{} {}\n", key, val));
         hash_string.push_str(&string);
-
     }
     let mut final_string_list = vec![];
     for char in input_string.chars() {
         let mut char = char;
-        if char == ' '{
+        if char == ' ' {
             char = '\u{2423}';
         }
         let code = &char_binary_codes[&char.to_string()];
@@ -122,8 +118,8 @@ fn encode() {
     }
     let final_string = final_string_list_strings.join("");
     let final_string_clone = final_string.clone();
-    input_to_file(final_string_clone,String::from("rustmanmsg"));
-    input_to_file(hash_string,String::from("rustmanhash"));
+    input_to_file(final_string_clone, String::from(format!("{}msg",path)));
+    input_to_file(hash_string, String::from(format!("{}hash",path)));
     println!("{} {}", final_string, final_string.len());
     let mut final_string_int = vec![];
     for num in final_string.chars() {
@@ -149,53 +145,19 @@ fn single_char(og_string: &String) -> Vec<char> {
     list_clone
 }
 
-fn decode (){
-    let mut unique_chars = String::new();
+fn decode(path:String) {
     let mut binary_codes: HashMap<String, char> = HashMap::new();
-    println!("Would you like to enter hash manually (m) or read hash file (r) ?");
-    let mut choice = String::new();
-    io::stdin().read_line(&mut choice).expect("Failed to read line");
-    if choice.trim() == "m"{
-        println!("How many unique characters in your encoded message?");
-        io::stdin().read_line(&mut unique_chars).expect("Failed to read line");
-        for _i in 1..=unique_chars.trim().parse::<i128>().unwrap() {
-            let mut char = String::new();
-            let mut binary_code = String::new();
-            println!("What's the character?");
-            io::stdin().read_line(&mut char).expect("Failed to read line");
-            if char.trim().chars().count() == 0 {
-                char = '\u{2423}'.to_string();
-            }
-            let char = char.trim();
-            println!("What's the binary code for {}?",char);
-            io::stdin().read_line(&mut binary_code).expect("Failed to read line");
-            let key = binary_code.trim().to_string();
-            binary_codes.insert(key, char.parse::<char>().unwrap());
-        }
-    } else {
-        let hash_as_string = fs::read_to_string("rustmanhash.txt").expect("Failed to read rustmanhash.txt");
-        for lines in hash_as_string.lines() {
+    let hash_as_string = fs::read_to_string(String::from(format!("{}hash.txt",path))).expect("Failed to read hash file");
+    for lines in hash_as_string.lines() {
             let list_of_chars = lines.chars().collect::<Vec<char>>();
             let letter = list_of_chars[0];
             let mut bc = String::new();
-            for char in 2..list_of_chars.len(){
+            for char in 2..list_of_chars.len() {
                 bc.push(list_of_chars[char]);
             }
             binary_codes.insert(bc, letter);
         }
-    }
-    let mut base = String::new();
-    let mut encoded = String::new();
-    println!("Would you like to enter your encoded message in base 2 (2) or read from file (r)");
-    io::stdin().read_line(&mut base).expect("Failed to read line");
-    if base.trim() == "2" {
-        println!("Please enter your encoded message");
-        io::stdin().read_line(&mut encoded).expect("Failed to read line");
-    }
-     else if base.trim() == "r"{
-        encoded = fs::read_to_string("rustmanmsg.txt").expect("Failed to read rustmanmsg.txt");
-    }
-    let encoded = encoded.trim();
+    let encoded = fs::read_to_string(String::from(format!("{}msg.txt",path))).expect("Failed to read msg file");
     let mut message = String::new();
     let mut current_string = String::new();
     for char in encoded.chars() {
@@ -203,7 +165,7 @@ fn decode (){
         let current_ref = &current_string;
         if binary_codes.contains_key(current_ref) {
             let mut letter = binary_codes[&current_string];
-            if letter == '\u{2423}'{
+            if letter == '\u{2423}' {
                 letter = ' ';
             }
             message.push(letter);
@@ -211,17 +173,21 @@ fn decode (){
         }
     }
     println!("{}", message);
-
 }
 fn order_by_value_list(list: Vec<Vec<String>>) -> Vec<Vec<String>> {
     let mut list_mut = list.clone();
     list_mut.sort_by(|a, b| a[0].cmp(&b[0]));
-    list_mut.sort_by(|a, b| a[1].trim().parse::<i32>().unwrap().cmp(&b[1].trim().parse::<i32>().unwrap()));
+    list_mut.sort_by(|a, b| {
+        a[1].trim()
+            .parse::<i32>()
+            .unwrap()
+            .cmp(&b[1].trim().parse::<i32>().unwrap())
+    });
     list_mut.reverse();
     list_mut
 }
 
-fn input_to_file(input:String, path:String) {
-    let mut file = File::create(String::from(format!("{}.txt",path))).unwrap();
+fn input_to_file(input: String, path: String) {
+    let mut file = File::create(String::from(format!("{}.txt", path))).unwrap();
     file.write_all(input.as_bytes()).unwrap();
 }
